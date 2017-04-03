3bf868b81c9c6cf05c64c0a6aa2af15550451585
dominic park <realslow8208@naver.com>
2017-04-03 14:52:26 +0900

    d

--- nfit/src/main/java/controller/IndexController.java
+++ nfit/src/main/java/controller/IndexController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-	@RequestMapping("/index.do")
+	@RequestMapping("index.do")
 	public String index(){
-		return "/index.jsp";
+		return "index.jsp";
 	}
 }
