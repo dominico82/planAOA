f309d2c5b09ab6514f3242377fa49b270043f45c
wonjun <wonjun@DESKTOP-0169VO9.mshome.net>
2017-04-18 15:44:23 +0900

    wonjun

--- nfit/src/main/java/nfit/member/model/MemberDAOImple.java
+++ nfit/src/main/java/nfit/member/model/MemberDAOImple.java
@@ -1,9 +1,12 @@
 package nfit.member.model;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
 public class MemberDAOImple implements MemberDAO {
 
@@ -115,6 +118,21 @@ public class MemberDAOImple implements MemberDAO {
 			return result;
 		}
 		
+	}
+	
+	public int setImage(MemberDTO member_idx, List<MultipartFile> files) throws IllegalStateException, IOException {
+		
+		String savePath = "C:/Users/wonjun/git/planAOA/nfit/src/main/webapp/resources/upload_images";
+		
+		int count = sqlMap.insert("setUpload", member_idx);
+		
+		for(int i=0; i<files.size(); i++){
+//			System.out.println("사진 이름: "+files.get(i).getOriginalFilename());
+			String fileName=files.get(i).getOriginalFilename();//파일을 저장하기 위해 이름을 가져온다.
+			files.get(i).transferTo(new File(savePath+fileName));//파일을 업로드한다.
+			int fcount=sqlMap.insert("setNoticePics", fileName);//DB에 사진 이름을 저장한다.
+		}
+		 return count;
 	}
 
 	/*
